33d4428e29b5fe8f07e9b29fcf4200443c3f8712
LucasTesBIOMED <lucas.biomecgmail.com>
2024-11-08 11:25:58 +0100

    ex changes

--- src/executables/train-pretrained.sh
+++ src/executables/train-pretrained.sh
@@ -4,5 +4,5 @@
 source /mnt/cephfs/home/amb/ltesan/miniconda3/bin/activate pl
 
 # Execute the Python script with train argument and additional training parameters
-python main.py --train --dim_hidden 250 --passes 12 --lambda_d 5. --max_epoch 5000  --batch_size 8
+python main.py  --pretrained  --train --dim_hidden 250 --passes 12 --lambda_d 5. --max_epoch 5000  --batch_size 8  --lr 0.0001
 
